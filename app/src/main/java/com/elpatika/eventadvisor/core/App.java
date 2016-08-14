40fb450b0e8a6c9e3b32e4764163b59964337069
Kirill Makarov <kir-maka@yandex.ru>
2016-08-14 12:09:11 +0300

    add static link to app in App

--- app/src/main/java/com/elpatika/eventadvisor/core/App.java
+++ app/src/main/java/com/elpatika/eventadvisor/core/App.java
@@ -1,6 +1,26 @@
 package com.elpatika.eventadvisor.core;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
 
 public class App extends Application {
+
+    private static App application;
+
+    @NonNull
+    public static App get(@NonNull Context context) {
+        return (App) application;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init();
+    }
+
+    private void init() {
+        application = this;
+    }
 }
